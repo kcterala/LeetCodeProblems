1fc369492a04f2de4d6286d8c74b272f74bd8aaa
Terala Krishna Chaitanya <78842299+kcterala@users.noreply.github.com>
2022-07-29 08:11:32 +0530

    Create findAndReplacePattern.java
    29 July 2022

---
+++ findAndReplacePattern.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        String p = checkString(pattern);
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<words.length;i++){
+            if(pattern.equals(words[i]) || p.equals(checkString(words[i]))){
+                list.add(words[i]);
+            }
+        }
+        return list;
+        
+    }
+    private static String checkString(String pattern){
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        int count = 0;
+        StringBuilder check = new StringBuilder("");
+        for(int i=0;i<pattern.length();i++){
+            char ch = pattern.charAt(i);
+            if(!map.containsKey(ch)){
+                map.put(ch,count);
+                count++;
+            }
+            check.append(map.get(pattern.charAt(i)));
+        }
+        return check.toString();
+    }
+}
